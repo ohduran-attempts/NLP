ad8f5e0cdf4ab7d01ab268df2b8d85f8ccb21ad6
ohduran <alvaro.duranb@gmail.com>
2017-07-01 23:04:53 +0100

    Fourth draft

--- sentiment_analysis_2.py
+++ sentiment_analysis_2.py
@@ -1,42 +1,129 @@
-"""New data set training."""
 import nltk
-from nltk.tokenize import sent_tokenize, word_tokenize
-# Now, we can build our data using .txt files
-# in a similar way.
+import random
+from nltk.corpus import movie_reviews
+from nltk.classify.scikitlearn import SklearnClassifier
+import pickle
+
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
+
+from nltk.classify import ClassifierI
+from statistics import mode
+
+from nltk.tokenize import word_tokenize
 
 
-def documenting(text1, text2):
-    """Create the document list."""
-    documents = []
-    for r in text1.split('/n'):
-        documents.append((r, 'pos'))
-    for r in text2.split('/n'):
-        documents.append((r, 'pos'))
-    return documents
-    # Now, all words. Same as before.
+class VoteClassifier(ClassifierI):
+    """Classifier."""
+
+    def __init__(self, *classifiers):
+        """Init method."""
+        self._classifiers = classifiers
+
+    def classify(self, features):
+        """Classify method."""
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+        return mode(votes)
+
+    def confidence(self, features):
+        """Confidence method."""
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+
+        choice_votes = votes.count(mode(votes))
+        conf = choice_votes / len(votes)
+        return conf
 
 
-def all_wording(text1, text2):
-    """Create the all_words."""
-    all_words = []
-    short_pos_words = word_tokenize(text1)
-    short_neg_words = word_tokenize(text2)
+short_pos = open("review_text/positive.txt", "r").read().decode('utf-8')
+short_neg = open("review_text/negative.txt", "r").read().decode('utf-8')
 
-    for w in short_pos_words:
-        all_words.append(w.lower())
-    for w in short_neg_words:
-        all_words.append(w.lower())
+documents = []
 
-    return nltk.FreqDist(all_words)
+for r in short_pos.split('\n'):
+    documents.append((r, "pos"))
+
+for r in short_neg.split('\n'):
+    documents.append((r, "neg"))
 
 
-with open("review_text/positive.txt", "r") as f:
-    short_pos = f.read
+all_words = []
 
-with open("review_text/negative.txt", "r") as f:
-    short_neg = f.read
+short_pos_words = word_tokenize(short_pos)
+short_neg_words = word_tokenize(short_neg)
 
-all_words = all_wording(short_pos, short_neg)
-documents = documenting(short_pos, short_neg)
+for w in short_pos_words:
+    all_words.append(w.lower())
+
+for w in short_neg_words:
+    all_words.append(w.lower())
+
+all_words = nltk.FreqDist(all_words)
 
 word_features = list(all_words.keys())[:5000]
+
+
+def find_features(document):
+    """Find features."""
+    words = word_tokenize(document)
+    features = {}
+    for w in word_features:
+        features[w] = (w in words)
+
+    return features
+
+featuresets = [(find_features(rev), category) for (rev, category) in documents]
+random.shuffle(featuresets)
+
+# data example:
+training_set = featuresets[:10000]
+testing_set = featuresets[10000:]
+
+#
+# classifier = nltk.NaiveBayesClassifier.train(training_set)
+# print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
+# classifier.show_most_informative_features(15)
+#
+# MNB_classifier = SklearnClassifier(MultinomialNB())
+# MNB_classifier.train(training_set)
+# print("MNB_classifier accuracy percent:", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
+#
+# BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
+# BernoulliNB_classifier.train(training_set)
+# print("BernoulliNB_classifier accuracy percent:", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
+#
+# LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+# LogisticRegression_classifier.train(training_set)
+# print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
+#
+# SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
+# SGDClassifier_classifier.train(training_set)
+# print("SGDClassifier_classifier accuracy percent:", (nltk.classify.accuracy(SGDClassifier_classifier, testing_set))*100)
+#
+# ##SVC_classifier = SklearnClassifier(SVC())
+# ##SVC_classifier.train(training_set)
+# ##print("SVC_classifier accuracy percent:", (nltk.classify.accuracy(SVC_classifier, testing_set))*100)
+#
+# LinearSVC_classifier = SklearnClassifier(LinearSVC())
+# LinearSVC_classifier.train(training_set)
+# print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
+#
+# NuSVC_classifier = SklearnClassifier(NuSVC())
+# NuSVC_classifier.train(training_set)
+# print("NuSVC_classifier accuracy percent:", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
+#
+#
+# voted_classifier = VoteClassifier(
+#                                   NuSVC_classifier,
+#                                   LinearSVC_classifier,
+#                                   MNB_classifier,
+#                                   BernoulliNB_classifier,
+#                                   LogisticRegression_classifier)
+#
+# print("voted_classifier accuracy percent:", (nltk.classify.accuracy(voted_classifier, testing_set))*100)
